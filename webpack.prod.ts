c17ff4435c7b6757eef03292dc69ca4639d7c1c6
manuel <manuel.payan@pccomponentes.com>
2020-03-29 13:47:18 +0200

    add webpack prod

---
+++ webpack.prod.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [],
+};
+
+export default config;
