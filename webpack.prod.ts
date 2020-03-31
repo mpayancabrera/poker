23d94efb49df157a24ed81f5f4e1d5c19d70553e
manuel <manuel.payan@pccomponentes.com>
2020-03-31 12:44:14 +0200

    Update image size

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,8 +1,7 @@
 import path from "path";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import CopyWebPackPlugin from "copy-webpack-plugin";
-import ImageMinWebpackPlugin from "imagemin-webpack-plugin";
-import ImageMinMozJpeg from "imagemin-webpack-plugin";
+import ImageminPlugin from "imagemin-webpack-plugin";
 import webpack from "webpack";
 
 const htmlPlugin = new HtmlWebPackPlugin({
@@ -47,9 +46,8 @@ const config: webpack.Configuration = {
         to: "images"
       }
     ]),
-    new ImageMinWebpackPlugin({
-      pngquant: { quality: "60-80" },
-      jpegtran: { progressive: false }
+    new ImageminPlugin({
+      pngquant: { quality: [0.5, 0.5] }
     })
   ]
 };
