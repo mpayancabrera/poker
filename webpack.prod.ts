e20a76ef2c805167149db4a1152e61d2b6d8a6aa
manuel <manuel.payan@pccomponentes.com>
2020-03-31 10:59:28 +0200

    Optimize images

--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,48 +1,57 @@
-import path from 'path';
+import path from "path";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import CopyWebPackPlugin from "copy-webpack-plugin";
-import webpack from 'webpack';
+import ImageMinWebpackPlugin from "imagemin-webpack-plugin";
+import ImageMinMozJpeg from "imagemin-webpack-plugin";
+import webpack from "webpack";
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: "./public/index.html"
 });
 
 const config: webpack.Configuration = {
-  mode: 'production',
-  entry: './src/index.tsx',
+  mode: "production",
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader',
+        loader: "awesome-typescript-loader"
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
-  plugins: [htmlPlugin, new CopyWebPackPlugin([
-    {
-      from: "./public/css",
-      to: "css"
-    },
-    {
-      from: "./public/js",
-      to: "js"
-    },
-    {
-      from: "./public/images",
-      to: "images"
-    }
-  ])],
+  plugins: [
+    htmlPlugin,
+    new CopyWebPackPlugin([
+      {
+        from: "./public/css",
+        to: "css"
+      },
+      {
+        from: "./public/js",
+        to: "js"
+      },
+      {
+        from: "./public/images",
+        to: "images"
+      }
+    ]),
+    new ImageMinWebpackPlugin({
+      pngquant: { quality: [0.7, 0.7] },
+      jpegtran: { progressive: true }
+    })
+  ]
 };
 
 export default config;
