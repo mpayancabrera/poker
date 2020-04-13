c9d30f263b124879c9f0e6ebbeb6ca5880a5e88f
manuel <manuel.payan@pccomponentes.com>
2020-04-13 16:38:17 +0200

    delete unused files

--- public/js/init.js
+++ public/js/init.js
@@ -96,22 +96,6 @@
       }
 
 	});
-
-
-/*----------------------------------------------------*/
-/*	Flexslider
-/*----------------------------------------------------*/
-   $('.flexslider').flexslider({
-      namespace: "flex-",
-      controlsContainer: ".flex-container",
-      animation: 'slide',
-      controlNav: true,
-      directionNav: false,
-      smoothHeight: true,
-      slideshowSpeed: 7000,
-      animationSpeed: 600,
-      randomize: false,
-   });
 });
 
 
